ec287ca53edec2d8203109f6c303e5100687f3ad
Sparisoma Viridi <dudung@gmail.com>
2022-05-08 08:48:16 +0700

    Create 02.js

---
+++ content/2022/05/02.js
@@ -0,0 +1,58 @@
+/*
+  2022_05_02.js
+  Anything hit the day
+  Sparisoma Viridi
+  http://github.com/dudung
+*/
+
+poem["2022-05-02"] =`
+Jalan terblok mobil di Kolam
+Menjaga jalan tetap lapang
+Ke Pusdai ternyata Gasibu
+Kecuali hujan boleh di sana
+
+Sudah dua tahun berlalu
+Tanpa sholat Ied di Gasibu
+Alhamdulillah keadaan membaik
+Dapat sholat bersama lagi
+
+Pusdai ke Gasibu
+Lewat Diponegoro
+Gasibu ke Pusdai
+Melalui Surapati
+
+Belum tiba di Rancaupas
+Masih empat kilo lagi
+Mampir Indomaret dulu
+Makan bekal menunggu buka
+
+Lebih sedikit jam sembilan
+Perut sudah bergemuruh
+Sambil santap bekal sarapan
+Sudah bisa beli minuman
+
+Kabut merengkuh bukit jauh
+Hujan rintik lalu deras
+Ciprat titik-titik hitam
+Sisa-sisa api unggun
+
+Cuci-cuci mangkuk pelastik
+Sampai tenda santap popmie
+Tambahkan rendang dan nasi
+Alhamdulillah menambah energi
+
+Kabut lebat menjelang maghrib
+Api-api unggun mulai dinyalakan
+Ke air lalu mushola
+Hujan angin kembali mengunjungi
+
+Sholat di mushola sholat di tenda
+Sholat di tenda sholat di mushola
+Berganti bergatung cuaca
+Musim panas hanya Juni-Agustus
+
+Pause machen wir jetzt?
+Tenda bocor lagi di ujung
+Belum tahu cara tangani
+Semoga hujan segera berhenti
+`;
